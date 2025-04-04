5bc69cc5f14c5acc5add9f87eeb5f2b969360bce
uzuw <uzuwxd@gmail.com>
2025-04-04 23:45:43 +0545

    mangas fetching

--- index.js
+++ index.js
@@ -13,6 +13,25 @@ app.get('/',(req,res)=>{
     res.send('mangadex api backend is running');
 })
 
+
+app.get('/api/recent-chapters',async(req,res)=>{
+    try{
+        const response=await axios.get('https://api.mangadex.org/chapter',{
+            params: {
+              limit: 10,
+              translatedLanguage: ['en'],
+              order: {
+                readableAt: 'desc'
+              }
+            }
+          });
+          res.json(response.data);
+        } catch (error) {
+          console.error('Error fetching recent chapters:', error.message);
+          res.status(500).json({ error: 'Failed to fetch recent chapters' });
+        }
+    });
+
 app.listen(PORT,()=>{
 console.log(`server is now running in https://localhost:${PORT}`);
 })
